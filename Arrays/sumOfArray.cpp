7916dd66e692ee1b85975d9ee2a665bb20311db2
Himanshu Joshi <hiimanshuhh16@gmail.com>
2025-02-17 23:30:38 +0530

    adding files

---
+++ Arrays/sumOfArray.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std; 
+int main(){
+  int n;
+  cout<<"Enter Your Array Size : ";
+  cin>>n;
+  int arr[n];
+  for(int i=0;i<n;i++){
+    cout<<"Enter Element : ";
+    cin>>arr[i];
+  }
+  for(int j=0;j<n;j++){
+    cout<<arr[j]<<" ";
+  }
+  cout<<endl;
+  int sum = 0;
+  for(int k=0;k<n;k++){
+    sum = sum+arr[k];
+  }
+  cout<<"The Sum Of Array Is : "<<sum;
+  return 0;
+}
