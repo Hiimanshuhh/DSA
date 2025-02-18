9fd9b8989c6fbf96600cd70c9cf03fc89c722f58
Himanshu Joshi <hiimanshuhh16@gmail.com>
2025-02-19 00:57:43 +0530

    adding files

---
+++ Arrays/largestElement.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int arr[] = {8,7,4,100,0,2,9,5};
+  int bigVal = 0;
+  for(int i=0;i<7;i++){
+    if(arr[i]>bigVal){
+      bigVal = arr[i];
+
+    }
+  }
+  cout<<bigVal;
+}
